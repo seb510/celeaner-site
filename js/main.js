1a62c985cdf669dcde15ab0fc353b20bd61b2fca
seb510 <sebistian@ukr.net>
2020-10-09 12:23:49 +0300

    add new file project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Скрипт для плавного перехода для якорных ссылок по сайту
+/*$(document).ready(function(){
+    $("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    }); 
+});
+*/
+//Header scroll
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//Navigation menu
+function toggleMenu() {
+    let menuToggle = document.querySelector('.toggle');
+    let menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+//Slider  
+$('.slider-wrapper').slick({
+	infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+	slidesToShow: 2,
+    responsive: [
+                {
+                  breakpoint: 980,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]    
+});
+
