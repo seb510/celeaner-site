0ad4e3d2e73ceba174f62b83b97cd03d1c84c9ba
seb510 <sebistian@ukr.net>
2020-11-13 21:53:09 +0200

    add php form

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //Header scroll
 $("body").on('click', '[href*="#"]', function(e){
     var fixed_offset = 140;
-    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1200);
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
     e.preventDefault();
 });
 
@@ -37,6 +37,7 @@ $('.owl-carousel').owlCarousel({
 })
 
 // Функция Modal Window
+
 $('.offer__btn').on('click', function () {
     $('.window').toggle();
 });
