5a4915d21a047f1130e702063647a79e8182b06a
seb510 <sebistian@ukr.net>
2020-12-09 20:40:33 +0200

    add light

--- js/main.js
+++ js/main.js
@@ -54,9 +54,11 @@ jQuery(function($){
     });
 });
 
+// Happy New Year
+//Snow
 jQuery(document).ready(function(){
     $(document).snowfall({
-        flakeCount: 100,
+        flakeCount: 80,
         image :"img/snow1.png",
         minSize: 10,
         maxSize:20,
@@ -64,6 +66,16 @@ jQuery(document).ready(function(){
         shadow: false,
     });
 });
+
+//Cristmas light
+function gir() {
+    nums = document.getElementById('nums_1').innerHTML
+    if(nums == 1) {document.getElementById('gir').className='gir_1';document.getElementById('nums_1').innerHTML='2'}
+    if(nums == 2) {document.getElementById('gir').className='gir_2';document.getElementById('nums_1').innerHTML='3'}
+    if(nums == 3) {document.getElementById('gir').className='gir_3';document.getElementById('nums_1').innerHTML='1'}
+}
+setInterval(function(){gir()}, 500)
+
 
 //Slider
 $('.owl-carousel').owlCarousel({
