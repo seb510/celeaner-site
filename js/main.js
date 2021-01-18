f6cc5f5aa7814d17fc7e046d1316700a6a9633c7
seb510 <sebistian@ukr.net>
2021-01-18 18:12:57 +0200

    dalete new year style

--- js/main.js
+++ js/main.js
@@ -14,51 +14,26 @@ function toggleMenu() {
 }
 
 //Ub button
-jQuery.extend(jQuery.fn, {
-    toplinkwidth: function(){
-        var totalContentWidth = jQuery('#content').outerWidth(); // ширина блока с контентом, включая padding
-        var totalTopLinkWidth = jQuery(this).children('a').outerWidth(true); // ширина самой кнопки наверх, включая padding и margin
-        var h = jQuery(window).width()/2-totalContentWidth/2-totalTopLinkWidth;
-        if(h<0){
-            // если кнопка не умещается, скрываем её
-            jQuery(this).hide();
-            return false;
-        } else {
-            if(jQuery(window).scrollTop() >= 1){
-                jQuery(this).show();
-            }
-            jQuery(this).css({'padding-right': h+'px'});
-            return true;
-        }
-    }
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
 });
 
-jQuery(function($){
-    var topLink = $('#top-link');
-    topLink.css({'padding-bottom': $(window).height()});
-    // если вам не нужно, чтобы кнопка подстраивалась под ширину экрана - удалите следующие четыре строчки в коде
-    topLink.toplinkwidth();
-    $(window).resize(function(){
-        topLink.toplinkwidth();
-    });
-    $(window).scroll(function() {
-        if($(window).scrollTop() >= 1 && topLink.toplinkwidth()) {
-            topLink.fadeIn(300);
-        } else {
-            topLink.fadeOut(300);
-        }
-    });
-    topLink.click(function(e) {
-        $("body,html").animate({scrollTop: 0}, 500);
-        return false;
-    });
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
 
 // Happy New Year
 //Snow
 jQuery(document).ready(function(){
     $(document).snowfall({
-        flakeCount: 60,
+        flakeCount: 30,
         image :"img/snow1.png",
         minSize: 5,
         maxSize:20,
