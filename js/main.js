4c6e24a1b4e9516956e72ffe71d94f82249e42c4
seb510 <sebistian@ukr.net>
2021-01-25 21:55:27 +0200

    new burger menu, remove style HN

--- js/main.js
+++ js/main.js
@@ -1,80 +1,69 @@
 //Header scroll
-$("body").on('click', '[href*="#"]', function(e){
+$("body").on('click', '[href*="#"]', function(e) {
     var fixed_offset = 140;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1500);
     e.preventDefault();
 });
 
 //Navigation menu
-document.querySelector('.menu__btn').addEventListener('click', function(){
+document.querySelector('.menu__btn').addEventListener('click', function() {
     document.querySelector('.menu__btn span').classList.toggle('active');
     document.querySelector('.menu').classList.toggle("animate");
-  });
+    $('.menu__list li a').click(function() {
+        if ($('.menu__btn span').hasClass('active')) {
+            $('.menu__btn span').removeClass('active');
+            $('.menu').removeClass("animate");
+        }
+    })
+});
 
-/*function toggleMenu() {
-    let menuToggle = document.querySelector('.toggle');
-    let menu = document.querySelector('.menu');
-    menuToggle.classList.toggle('active');
-    menu.classList.toggle('active');
-}
-*/
 //Ub button
 var btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
 });
 
 btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
 });
 
-// Happy New Year
-//Snow
-jQuery(document).ready(function(){
-    $(document).snowfall({
-        flakeCount: 30,
-        image :"img/snow1.png",
-        minSize: 5,
-        maxSize:20,
-        round: true,
-        shadow: false,
-    });
-});
 
 //Slider
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots: false,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    nav: false,
+    dots: true,
     navText: ["&lang;", "&rang;"],
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:2
+        1000: {
+            items: 2
         }
     }
 })
 
 // Функция Modal Window
-$('.offer__btn').on('click', function () {
+$('.offer__btn').on('click', function() {
     $('.window').fadeIn('slowly');
 });
 
-$('.window__closed').on('click', function () {
+$('.window__closed').on('click', function() {
     $('.window').slideUp('slowly');
 });
-
-
-
+$('.window__closed').on('click', function() {
+    $('.window').slideUp('slowly');
+});
